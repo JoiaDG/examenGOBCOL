575124806e616d03714576e246c3545766605aa3
JoiaDG <47857218+JoiaDG@users.noreply.github.com>
2019-09-24 15:11:54 -0500

    Palabra Palindroma funcionando

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var hora = document.querySelector("#hora");
+var boton = document.querySelector("#submit");
+
+boton.addEventListener('click', function(){
+    var frase = document.querySelector("#oracion").value.toString();
+    frase = frase.replace(/ /g, "")
+    var matriz = [];
+    var contador = parseInt(frase.length);
+    for(var i = 0; i <= contador; i++){
+        matriz.push(frase[i]);
+    }
+
+    var i=contador-1;
+    var valida = 0;
+    var lomismo = "";
+    for(var x = 0; x < contador; x++)
+    {
+        lomismo= lomismo +"Letra Ida: [" + matriz[x] + "] Letra vuelta: [" + matriz[i] + "] <br>";
+        if(matriz[x]===matriz[i]){
+            console.log(x+" Letra concuerda")
+            i--;
+            valida++;
+            continue;
+        }else{
+            alert("Tu palabra no es palindroma");
+            break;
+        }
+    }
+    document.querySelector(".palabra").innerHTML = lomismo
+    console.log(contador);
+    console.log(valida);
+    if(contador == valida){
+        alert("Tu palabra si es palindroma");
+    }
+    
+});
