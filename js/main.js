7d28dcfb094f85318ed12930c83a84fecd4fc8d3
JoiaDG <47857218+JoiaDG@users.noreply.github.com>
2019-09-27 09:47:23 -0500

    hola

---
+++ js/main.js
@@ -0,0 +1,43 @@
+document.querySelector("#hora").value = new Date();
+var boton = document.querySelector("#submit");
+boton.addEventListener('click', function(){
+    var frase = document.querySelector("#oracion").value.toString().toLowerCase();
+    frase = frase.replace(/ /g, "")
+    var matriz = [];
+    var contador = parseInt(frase.length);
+    for(var i = 0; i <= contador; i++){
+        matriz.push(frase[i]);
+    }
+
+    var i=contador-1;
+    var valida = 0;
+    var lomismo = "";
+    for(var x = 0; x < contador; x++)
+    {
+        lomismo= lomismo +"Letra Ida: [" + matriz[x] + "] Letra vuelta: [" + matriz[i] + "] <br>";
+        if(matriz[x]===matriz[i]){
+            console.log(x+" Letra concuerda")
+            i--;
+            valida++;
+            continue;
+        }else{
+            alert("Tu palabra no es palindroma");
+            break;
+        }
+    }
+    document.querySelector(".palabra").innerHTML = lomismo
+    console.log(contador);
+    console.log(valida);
+    if(contador == valida){
+        alert("Tu palabra si es palindroma");
+    }
+    
+
+
+});
+
+async function llamar() {
+    const data = await fetch("http://worldtimeapi.org/api/timezone/America/Argentina/Salta")
+                    .then(res => res.json())
+    document.querySelector("#hora").value = new Date(data.unixtime);
+}
